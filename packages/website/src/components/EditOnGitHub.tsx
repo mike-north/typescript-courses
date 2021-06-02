8f136804840f9aa54213a07a6255d477fe478496
Mike North <michael.l.north@gmail.com>
2021-06-01 21:14:27 -0700

    lint fixes

--- packages/website/src/components/EditOnGitHub.tsx
+++ packages/website/src/components/EditOnGitHub.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { path } from 'vfile';
-// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
 import githubIcon from '../../content/assets/GitHub-Mark-32px.png';
 
 export interface EditOnGitHubLinkProps {
@@ -9,16 +9,19 @@ export interface EditOnGitHubLinkProps {
 
 const REPO = 'mike-north/ts-fundamentals-v3';
 
-const EditOnGitHubLink: React.FunctionComponent<EditOnGitHubLinkProps> =
-  ({ chapter }): JSX.Element => {
-    const url = `https://github.com/${REPO}/edit/main/packages/website/content/blog/${chapter.replace('/course', '').substr(1)}index.md`;
-    return (
-      <a href={url} target="_blank">
-        <div className="edit-on-github-link">
-          <img src={githubIcon} alt="GitHub" />
-          Edit on GitHub
-        </div>
-      </a>
-    );
-  };
+const EditOnGitHubLink: React.FunctionComponent<EditOnGitHubLinkProps> = ({
+  chapter,
+}): JSX.Element => {
+  const url = `https://github.com/${REPO}/edit/main/packages/website/content/blog/${chapter
+    .replace('/course', '')
+    .substr(1)}index.md`;
+  return (
+    <a href={url} target="_blank">
+      <div className="edit-on-github-link">
+        <img src={githubIcon} alt="GitHub" />
+        Edit on GitHub
+      </div>
+    </a>
+  );
+};
 export default EditOnGitHubLink;
