481f6ebeb3eb48be6de08fec046fc4cf06910f94
Mike North <michael.l.north@gmail.com>
2021-06-15 12:16:27 -0700

    GTM

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -126,9 +126,37 @@ module.exports = {
       resolve: `gatsby-plugin-sharp`,
     },
     {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-PW24ZG5",
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+  
+        // // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+  
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+      },
+    },
+    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: ['G-NPMJTDV8W2', 'GTM-PW24ZG5'],
+        trackingIds: ['G-NPMJTDV8W2'],
       },
     },
     `gatsby-plugin-feed`,
