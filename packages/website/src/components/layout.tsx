f18f654cef69d38a3fb2771b818d70ffb608c03c
Mike North <michael.l.north@gmail.com>
2022-03-18 18:12:50 -0700

    increase content width

--- packages/website/src/components/layout.tsx
+++ packages/website/src/components/layout.tsx
@@ -62,7 +62,7 @@ const Layout: React.FunctionComponent<ILayoutProps> = ({
       style={{
         marginLeft: `auto`,
         marginRight: `auto`,
-        maxWidth: rhythm(24),
+        maxWidth: rhythm(30),
         padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
       }}
     >
