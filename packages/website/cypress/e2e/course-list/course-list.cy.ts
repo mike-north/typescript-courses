b96f7b3e4b2f62670b22d38859804719d0da7a2b
Mike North <michael.l.north@gmail.com>
2023-10-02 00:30:16 -0700

    Better cypress test names (#766)
    * cypress tests
    
    * Run cypress tests in first CI build stage
    
    * cypress config at root
    
    * Explicit specification of cypress config file in github workflow
    
    * remove explicit build before cypress tests run
    
    * fix path to cypress config file
    
    * Run cypress tests in first CI build stage
    
    * define cwd for cypress gha
    
    * run cypress on port :8000
    
    * fix two failing tests
    
    * better test names

--- packages/website/cypress/e2e/course-list/course-list.cy.ts
+++ packages/website/cypress/e2e/course-list/course-list.cy.ts
@@ -11,7 +11,7 @@
 // please read our getting started guide:
 // https://on.cypress.io/introduction-to-cypress
 
-describe('example to-do app', () => {
+describe('course list page', () => {
   beforeEach(() => {
     // Cypress starts out with a blank slate for each test
     // so we must tell it to visit our website with the `cy.visit()` command.
