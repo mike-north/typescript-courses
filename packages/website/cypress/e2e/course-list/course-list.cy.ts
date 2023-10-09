40bbaf5118289d49fb403c7d6f706fee9a9dcc53
Mike North <michael.l.north@gmail.com>
2023-10-08 21:36:03 -0700

    regression tests for top nav

--- packages/website/cypress/e2e/course-list/course-list.cy.ts
+++ packages/website/cypress/e2e/course-list/course-list.cy.ts
@@ -1,37 +1,12 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
-
 describe('course list page', () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
     cy.visit('http://localhost:8000').waitForRouteChange();
   });
 
   it('displays multiple courses', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
     cy.get('.course-summary').should('have.length', 4);
-
-    // We can go even further and check that the default todos each contain
-    // the correct text. We use the `first` and `last` functions
-    // to get just the first and last matched elements individually,
-    // and then perform an assertion with `should`.
-    // cy.get('.todo-list li').first().should('have.text', 'Pay electric bill')
-    // cy.get('.todo-list li').last().should('have.text', 'Walk the dog')
   });
 
   it('clicking TS-fundamentals v3 course link visits the course page', () => {
@@ -93,5 +68,10 @@ describe('course list page', () => {
       'have.length.at.least',
       5,
     );
+  });
+  it('course top nav has the correct courses', () => {
+    cy.get('.course-summary:nth-child(2) header h3 a').click();
+    cy.get('li.course-tab a').should("have.length", 4);
+    cy.get('li.course-tab a').should("contain.text", "TypeScript Fundamentals v3Intermediate TypeScript v1Making TypeScript StickFull Stack TypeScript");
   });
 });
