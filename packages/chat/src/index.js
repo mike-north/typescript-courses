4ee440943db688d579846f856d7d29ad1979f44d
Mike North <michael.l.north@gmail.com>
2023-10-10 21:30:08 -0700

    Update react

--- packages/chat/src/index.js
+++ packages/chat/src/index.js
@@ -1,12 +1,13 @@
 import 'regenerator-runtime/runtime';
 import * as React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './ui/App';
 
 function initializeReactApp() {
   const appContainer = document.getElementById('appContainer');
   if (!appContainer) throw new Error('No #appContainer found in DOM');
-  render(React.createElement(App), appContainer);
+  const root = createRoot(appContainer);
+  root.render(React.createElement(App), appContainer);
 }
 
 initializeReactApp();
