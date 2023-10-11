9ac44c80b42e51c167e475c1c904af87a3c45e52
Mike North <michael.l.north@gmail.com>
2023-10-10 23:53:45 -0700

    build watcher works

--- packages/chat/server/index.mjs
+++ packages/chat/server/index.mjs
@@ -13,7 +13,7 @@ const PKG_JSON_PATH = pkgUp.pkgUpSync();
 if (!PKG_JSON_PATH) throw new Error('Could not determine package.json path');
 console.log("Directory: " + PKG_JSON_PATH)
 const DIR_NAME = join(PKG_JSON_PATH, '..');
-
+const DIST_INDEX_HTML = join(DIR_NAME, "dist", "index.html")
 const app = e();
 
 setupAPI(server);
@@ -29,14 +29,28 @@ defaultTargetOptions: {
       browsers: ['last 1 Chrome version']
     }
   }})
+
 await bundler.watch((err, buildEvent) => {
-    console.log(`Parcel: `, buildEvent)
-})
+    if(buildEvent && buildEvent.type === "buildSuccess") {
+        const { buildTime } = buildEvent;
+        console.log(`🏗️ UI build complete: ${buildTime/1000}s`)
+        
+    }
+    if(err) console.error(err);
+});
+console.log("UI Build Watcher Started")
 
 app.use('/assets', e.static(join(DIR_NAME, 'assets')));
 app.use(server);
-console.log(join(DIR_NAME, "dist", "index.html"));
-app.get("/*", e.static(join(DIR_NAME, "dist")));
+app.get("*", (req, res, next) => {
+    if (req.path.endsWith(".js")) { next(); return; }
+    if (req.path.endsWith(".css")) { next(); return; }
+    if (req.accepts("*/html") === "*/html") {
+        res.sendFile(DIST_INDEX_HTML)
+    }
+    else next();
+});
+app.get("*", e.static(join(DIR_NAME, "dist")))
 
 // Listen on port PORT
 app.listen(PORT, () => {
