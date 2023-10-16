6bd4d014d533ce7ca2e2216aa6d9bcda2c004265
Mike North <michael.l.north@gmail.com>
2023-10-16 12:44:55 -0700

    Update Team.jsx

--- packages/chat/src/ui/components/Team.jsx
+++ packages/chat/src/ui/components/Team.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import SelectedChannel from './SelectedChannel'
 import TeamSidebar from './TeamSidebar'
 
 const Team = ({ team }) => {
