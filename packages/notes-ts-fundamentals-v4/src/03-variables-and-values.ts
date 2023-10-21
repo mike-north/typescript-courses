d0c6dcee9b25d1c99eed0a555732215b2f4541c7
Mike North <michael.l.north@gmail.com>
2023-10-20 20:07:39 -0700

    more notes

--- packages/notes-ts-fundamentals-v4/src/03-variables-and-values.ts
+++ packages/notes-ts-fundamentals-v4/src/03-variables-and-values.ts
@@ -1,41 +1,30 @@
 
-//! Variable Declarations & Inference
-export let temperature = 6
-// temperature = "warm"
-// const humidity = 79
-// temperature = 23
+//* Variable Declarations & Inference
+export let temperature = 6 //! inference
+// temperature = "warm"  //! type-checking
+// const humidity = 79  //! literal type
 
-// temperature = 23 // (1)
-// temperature = humidity; // (2)
-// humidity = temperature; // (3)
-// humidity = 79; // (4)
-// humidity = 78; // (5)
+//* A type as a set of allowed values
 
 
-//! A type as a set of allowed values
+/*
+// temperature = 23 //✔️ (1)  - re-assignability of a let
+// temperature = humidity; //! (2) - type-checking
+// humidity = temperature; //! (3) - number is not of type `79`
+// humidity = 79; //✔️ (4) - 79 is of type `79`
+// humidity = 78; //! (5) - 78 is not of type `79`
 
-// temp2's type is { all numbers }
-// let temp2 = 19;
+/*
+// let temp2 = 19; //! temp2's type is { all numbers }
+// let humid2 = 79 as const; //! humidity's type is { 79 }
+// temp2 = 23; //! Is each member in { 23 } also in { all numbers }?
+// temp2 = humid2; //! Is each member in { 79 } also in { all numbers }?
+// humid2 = temp2; //! Is each member in { all numbers } also in { 79 }?
+// humid2 = 79; //! Is each member in { 79 } also in { 79 }
+// humid2 = 78; //! Is each member in { 78 } also in { 79 }
 
-// let humid2 = 79 as const; // humidity's type is { 79 }
-
-// Is each member in { 23 } also in { all numbers }?
-// temp2 = 23;
-
-// Is each member in { 79 } also in { all numbers }?
-// temp2 = humid2;
-
-// Is each member in { all numbers } also in { 79 }?
-// humid2 = temp2;
-
-// Is each member in { 79 } also in { 79 }
-// humid2 = 79;
-
-// Is each member in { 78 } also in { 79 }
-// humid2 = 78;
-
-//! Implicit `any` and type annotations
-
+//* Implicit `any` and type annotations
+/*
 // between 500 and 1000
 // export const RANDOM_WAIT_TIME =
 //     Math.round(Math.random() * 500) + 500
@@ -49,26 +38,35 @@ export let temperature = 6
 // }, RANDOM_WAIT_TIME)
 
 
-//! Type Casting
-
+//* Type Casting
+/*
 // let frontEndMastersFounding = new Date("Jan 1, 2012")
 // let date1 = frontEndMastersFounding
 // let date2 = frontEndMastersFounding as any;
 
-
-// is 79 a number? If so, this is safe!
-// const humid3 = 79 as number; 
-
+/*
+// const humid3 = 79 as number; //✔️ is 79 a number? If so, this is safe!
 
 
+/*
 // let date3 = "oops" as any as Date
-// TypeScript thinks this is a Date now, but it's really a string
+//// TypeScript thinks this is a Date now, but it's really a string
 // date3
-// what do we think will happen when we run this? 💥
+//// what do we think will happen when we run this? 💥
 // date3.toISOString()
 
-
+/*
 // let date4 = "oops" as Date
 
 
 //! Function arguments and return values
+
+
+// function add(a, b) {
+//   return a + b // strings? numbers? a mix?
+// }
+
+// const result = add(3, "4")
+// const p = new Promise(result);
+
+/**/
---
+++ packages/notes-ts-fundamentals-v4/src/06-union-and-intersection-types.ts
@@ -0,0 +1,7 @@
+//* Union types in TypeScript
+
+//// const humidity = 79
+
+//// type OneThroughFive = 1 | 2 | 3 | 4 | 5
+//// let upToFive: OneThroughFive = 3
+//// upToFive = 8
