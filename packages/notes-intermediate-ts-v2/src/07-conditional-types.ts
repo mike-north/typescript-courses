6ac98725ffcdb057f81e4ef4d404b02e8789a067
Mike North <michael.l.north@gmail.com>
2023-10-22 18:07:46 -0500

    WIP

--- packages/notes-intermediate-ts-v2/src/07-conditional-types.ts
+++ packages/notes-intermediate-ts-v2/src/07-conditional-types.ts
@@ -18,5 +18,37 @@ let device1: CookingDevice<'grill'>
 let device2: CookingDevice<'oven'>
 //   ^?
 
+//
+const one = 1
+const two = 2
+const ten = 10
+
+type IsLowNumber<T> = T extends 1 | 2 ? true : false
+type TestOne = IsLowNumber<1>
+type TestTwo = IsLowNumber<2>
+type TestTen = IsLowNumber<10>
+type TestTenWithTwo = IsLowNumber<10 | 2>
+
+//
+// a set of four specific things
+type FavoriteColors =
+  | 'dark sienna'
+  | 'van dyke brown'
+  | 'yellow ochre'
+  | 'sap green'
+  | 'titanium white'
+  | 'phthalo green'
+  | 'prussian blue'
+  | 'cadium yellow'
+  | [number, number, number]
+  | { red: number; green: number; blue: number }
+
+type StringColors = Extract<FavoriteColors, string>
+//    ^?
+type ObjectColors = Extract<FavoriteColors, { red: number }>
+//    ^?
+type TupleColors =
+  //     ^?
+  Extract<FavoriteColors, [number, number, number]>
 /**/
 export default {}
