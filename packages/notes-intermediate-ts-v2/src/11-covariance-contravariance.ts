c2a64a42e03cea44e2fd9bb6937ec059a11c11d8
Mike North <michael.l.north@gmail.com>
2023-10-25 20:55:01 -0500

    11-covariance-contravariance-invariance

--- packages/notes-intermediate-ts-v2/src/11-covariance-contravariance.ts
+++ packages/notes-intermediate-ts-v2/src/11-covariance-contravariance.ts
@@ -18,109 +18,109 @@ class Cookie extends Snack {
 }
 
 //* Covariance
-/*
-// interface Producer<T> {
-//   produce: () => T
-// }
 
-// let cookieProducer: Producer<Cookie> = {
-//   produce: () => new Cookie('dark'),
-// }
-// let snackProducer: Producer<Snack> = {
-//   produce: () => Math.random() > 0.5 ? new Cookie("milk") : new Pretzel(true)
-// };
+interface Producer<T> {
+  produce: () => T
+}
 
-/*
-// //? Type equivalence check
-// snackProducer = cookieProducer //✔️ OK
-// cookieProducer = snackProducer //! Nope
+let cookieProducer: Producer<Cookie> = {
+  produce: () => new Cookie('dark'),
+}
+let snackProducer: Producer<Snack> = {
+  produce: () => Math.random() > 0.5 ? new Cookie("milk") : new Pretzel(true)
+};
 
-/*
+
+//? Type equivalence check
+snackProducer = cookieProducer //✔️ OK
+cookieProducer = snackProducer //! Nope
+
+
 // | Cookie                | direction     | Snack                 |
 // |-----------------------|---------------|-----------------------|
 // | `Cookie`              | --- is a ---> | `Snack`               |
 // | `Producer<Cookie>`    | --- is a ---> | `Producer<Snack>`     |
 
-/*
-// interface Producer<out T> {
-//   produce: () => T
-// }
+
+interface Producer<out T> {
+  produce: () => T
+}
 
 //* Contravariance
-/*
-// interface Packager<T> {
-//   package: (item: T) => void
-// }
 
-// let cookiePackager: Packager<Cookie> = {
-//   package(item: Cookie) {},
-// }
+interface Packager<T> {
+  package: (item: T) => void
+}
 
-// let snackPackager: Packager<Snack> = {
-//   package(item: Snack) {
-//     if (item instanceof Cookie) {
-//        // Package cookie
-//     } else if (item instanceof Pretzel) {
-//        // Package pretzel
-//     } else {
-//        // Package other snacks?
-//     }
-//   },
-// }
+let cookiePackager: Packager<Cookie> = {
+  package(item: Cookie) {},
+}
 
-/*
-// //? Type equivalence check
-// cookiePackager = snackPackager //✔️ OK
-// snackPackager = cookiePackager //! Nope
+let snackPackager: Packager<Snack> = {
+  package(item: Snack) {
+    if (item instanceof Cookie) {
+       // Package cookie
+    } else if (item instanceof Pretzel) {
+       // Package pretzel
+    } else {
+       // Package other snacks?
+    }
+  },
+}
 
-/*
+
+//? Type equivalence check
+cookiePackager = snackPackager //✔️ OK
+snackPackager = cookiePackager //! Nope
+
+
 // | Cookie                | direction     | Snack                 |
 // |-----------------------|---------------|-----------------------|
 // | `Cookie`              | --- is a ---> | `Snack`               |
 // | `Packager<Cookie>`    | <--- is a --- | `Packager<Snack>`     |
 
-/*
-// interface Packager<in T> {
-//   package: (item: T) => void
-// }
+
+interface Packager<in T> {
+  package: (item: T) => void
+}
 
 //* Invariance
-/*
-// interface ProducerPackager<T> {
-//   produce: () => T
-//   package: (item: T) => void
-// }
-/*
-// let cookieProducerPackager: ProducerPackager<Cookie> = {
-//   produce() {
-//     return new Cookie('dark')
-//   },
-//   package(arg: Cookie) {},
-// }
 
-// let snackProducerPackager: ProducerPackager<Snack> = {
-//   produce() {
-//     return Math.random() > 0.5
-//       ? new Cookie('milk')
-//       : new Pretzel(true)
-//   },
-//   package(item: Snack) {
-//     if (item instanceof Cookie) {
-//       /* Package cookie */
-//     } else if (item instanceof Pretzel) {
-//       /* Package pretzel */
-//     } else {
-//       /* Package other snacks? */
-//     }
-//   },
-// }
+interface ProducerPackager<T> {
+  produce: () => T
+  package: (item: T) => void
+}
 
-/*
-// //? Type equivalence check
-// cookieProducerPackager = snackProducerPackager
-// snackProducerPackager = cookieProducerPackager
+let cookieProducerPackager: ProducerPackager<Cookie> = {
+  produce() {
+    return new Cookie('dark')
+  },
+  package(arg: Cookie) {},
+}
 
-/*
+let snackProducerPackager: ProducerPackager<Snack> = {
+  produce() {
+    return Math.random() > 0.5
+      ? new Cookie('milk')
+      : new Pretzel(true)
+  },
+  package(item: Snack) {
+    if (item instanceof Cookie) {
+      /* Package cookie */
+    } else if (item instanceof Pretzel) {
+      /* Package pretzel */
+    } else {
+      /* Package other snacks? */
+    }
+  },
+}
+
+
+//? Type equivalence check
+cookieProducerPackager = snackProducerPackager
+snackProducerPackager = cookieProducerPackager
+
+
 // | Cookie                    | direction     | Snack                 |
 // |---------------------------|---------------|-----------------------|
 // | `Cookie`                  | --- is a ---> | `Snack`               |
@@ -128,62 +128,62 @@ class Cookie extends Snack {
 
 //* Bivariance
 
-/*
-// function cookieQualityCheck(cookie: Cookie): boolean {
-//   return Math.random() > 0.1
-// }
 
-// function snackQualityCheck(snack: Snack): boolean {
-//   if (snack instanceof Cookie) return cookieQualityCheck(snack)
-//   else return Math.random() > 0.16 // pretzel case
-// }
+function cookieQualityCheck(cookie: Cookie): boolean {
+  return Math.random() > 0.1
+}
 
-/*
-// // A function type for preparing a bunch of food items
-// // for shipment. The function must be passed a callback
-// // that will be used to check the quality of each item.
-// type PrepareFoodPackage<T> = (
-//   uncheckedItems: T[],
-//   qualityCheck: (arg: T) => boolean,
-// ) => T[]
+function snackQualityCheck(snack: Snack): boolean {
+  if (snack instanceof Cookie) return cookieQualityCheck(snack)
+  else return Math.random() > 0.16 // pretzel case
+}
 
-/*
-// // Prepare a bunch of snacks for shipment
-// let prepareSnacks: PrepareFoodPackage<Snack> = (
-//   uncheckedItems,
-//   callback,
-// ) => uncheckedItems.filter(callback)
 
-// // Prepare a bunch of cookies for shipment
-// let prepareCookies: PrepareFoodPackage<Cookie> = (
-//   uncheckedItems,
-//   callback,
-// ) => uncheckedItems.filter(callback)
+// A function type for preparing a bunch of food items
+// for shipment. The function must be passed a callback
+// that will be used to check the quality of each item.
+type PrepareFoodPackage<T> = (
+  uncheckedItems: T[],
+  qualityCheck: (arg: T) => boolean,
+) => T[]
 
-/*
-// const cookies = [
-//   new Cookie('dark'),
-//   new Cookie('milk'),
-//   new Cookie('white'),
-// ]
-// const snacks = [
-//   new Pretzel(true),
-//   new Cookie('milk'),
-//   new Cookie('white'),
-// ]
-// prepareSnacks(cookies, cookieQualityCheck)
-// prepareSnacks(snacks, cookieQualityCheck)
-// prepareCookies(cookies, snackQualityCheck)
 
-/*
+// Prepare a bunch of snacks for shipment
+let prepareSnacks: PrepareFoodPackage<Snack> = (
+  uncheckedItems,
+  callback,
+) => uncheckedItems.filter(callback)
+
+// Prepare a bunch of cookies for shipment
+let prepareCookies: PrepareFoodPackage<Cookie> = (
+  uncheckedItems,
+  callback,
+) => uncheckedItems.filter(callback)
+
+
+const cookies = [
+  new Cookie('dark'),
+  new Cookie('milk'),
+  new Cookie('white'),
+]
+const snacks = [
+  new Pretzel(true),
+  new Cookie('milk'),
+  new Cookie('white'),
+]
+prepareSnacks(cookies, cookieQualityCheck)
+prepareSnacks(snacks, cookieQualityCheck)
+prepareCookies(cookies, snackQualityCheck)
+
+
 //? What if we turn `strictFunctionTypes` on and off?
 
 //* What do variance helpers do for you?
-/*
-// interface Example<in T> {
-//   package: (item: T) => void
-//   // produce: () => T;
-// }
+
+interface Example<out T> {
+  package: (item: T) => void
+  produce: () => T;
+}
 
 /**/
 export default {}
