8093a1303514d8fd8e6fbac201ca6f50d371c65e
Nikolaa92 <Nandelkovic53@gmail.com>
2024-11-13 15:27:20 -0500

    type as a set of allowed values

--- packages/notes-ts-fundamentals-v4/src/03-variables-and-values.ts
+++ packages/notes-ts-fundamentals-v4/src/03-variables-and-values.ts
@@ -1,41 +1,37 @@
 //* Variable Declarations & Inference
 let temperature = 6 //! inference
-/*
-// temperature = "warm"  //! type-checking
-// const humidity = 79  //! literal type
+
+//temperature = 'warm' //! type-checking
+const humidity = 79 //! literal type
 
 //* A type as a set of allowed values
-/*
-// temperature = 23 //✔️ (1)  - re-assignability of a let
-// temperature = humidity; //! (2) - type-checking
-// humidity = temperature; //! (3) - number is not of type `79`
-// humidity = 79; //✔️ (4) - 79 is of type `79`
-// humidity = 78; //! (5) - 78 is not of type `79`
 
+temperature = 23 //✔️ (1)  - re-assignability of a let
+temperature = humidity //! (2) - type-checking
+// humidity = temperature //! (3) - number is not of type `79`
+// humidity = 79 //✔️ (4) - 79 is of type `79`
+// humidity = 78 //! (5) - 78 is not of type `79`
 
-/*
-// let temp2 = 19; //! temp2's type is { all numbers }
-// let humid2 = 79 as const; //! humidity's type is { 79 }
-// temp2 = 23; //! Is each member in { 23 } also in { all numbers }?
-// temp2 = humid2; //! Is each member in { 79 } also in { all numbers }?
-// humid2 = temp2; //! Is each member in { all numbers } also in { 79 }?
-// humid2 = 79; //! Is each member in { 79 } also in { 79 }
-// humid2 = 78; //! Is each member in { 78 } also in { 79 }
+let temp2 = 19 //! temp2's type is { all numbers }
+let humid2 = 79 as const //! humidity's type is { 79 }
+temp2 = 23 //! Is each member in { 23 } also in { all numbers }?
+temp2 = humid2 //! Is each member in { 79 } also in { all numbers }?
+//humid2 = temp2 //! Is each member in { all numbers } also in { 79 }?
+humid2 = 79 //! Is each member in { 79 } also in { 79 }
+//humid2 = 78 //! Is each member in { 78 } also in { 79 }
 
 //* Implicit `any` and type annotations
-/*
-// between 500 and 1000
-// export const RANDOM_WAIT_TIME =
-//     Math.round(Math.random() * 500) + 500
 
-// let startTime = new Date()
-// let endTime
+//between 500 and 1000
+export const RANDOM_WAIT_TIME = Math.round(Math.random() * 500) + 500
 
-// setTimeout(() => {
-//     endTime = 0
-//     endTime = new Date()
-// }, RANDOM_WAIT_TIME)
+let startTime = new Date()
+let endTime: Date
 
+setTimeout(() => {
+  //endTime = 0
+  endTime = new Date()
+}, RANDOM_WAIT_TIME)
 
 //* Type Casting
 /*
