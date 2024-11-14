e4329845191c391f9059a74026f4d9d62b176eba
Nikolaa92 <Nandelkovic53@gmail.com>
2024-11-13 20:50:23 -0500

    Typing objects

--- packages/notes-ts-fundamentals-v4/src/03-variables-and-values.ts
+++ packages/notes-ts-fundamentals-v4/src/03-variables-and-values.ts
@@ -34,32 +34,26 @@ setTimeout(() => {
 }, RANDOM_WAIT_TIME)
 
 //* Type Casting
-/*
-// let frontEndMastersFounding = new Date("Jan 1, 2012")
-// let date1 = frontEndMastersFounding
-// let date2 = frontEndMastersFounding as any;
 
-/*
-// const humid3 = 79 as number; //✔️ is 79 a number? If so, this is safe!
+let frontEndMastersFounding = new Date('Jan 1, 2012')
+let date1 = frontEndMastersFounding
+let date2 = frontEndMastersFounding as any
 
+const humid3 = 79 as number //✔️ is 79 a number? If so, this is safe!
 
-/*
-// let date3 = "oops" as any as Date //! TypeScript thinks this is a Date now, but it's really a string
-// date3.toISOString() //! what do we think will happen when we run this? 💥
+let date3 = 'oops' as any as Date //! TypeScript thinks this is a Date now, but it's really a string
+date3.toISOString() //! what do we think will happen when we run this? 💥
 
-/*
-// let date4 = "oops" as Date
-
+//let date4 = 'oops' as Date
 
 //! Function arguments and return values
 
+function add(a: number, b: number): number {
+  return a + b // strings? numbers? a mix?
+}
 
-// function add(a, b) {
-//   return a + b // strings? numbers? a mix?
-// }
-
-// const result = add(3, "4")
-// const p = new Promise(result);
+const result = add(3, 4)
+//const p = new Promise(result);
 
 /**/
 
--- packages/notes-ts-fundamentals-v4/src/04-objects-arrays-and-tuples.ts
+++ packages/notes-ts-fundamentals-v4/src/04-objects-arrays-and-tuples.ts
@@ -1,44 +1,55 @@
 //* Objects
 
+const myCar = {
+  make: 'Toyota',
+  model: 'Corola',
+  year: 2002,
+}
+
 let car: {
   make: string
   model: string
   year: number
+} = myCar
+
+//? A function that prints info about a car to stdout
+function printCar(car: {
+  make: string
+  model: string
+  year: number
+  chargeVoltage?: number
+}) {
+  let str = `${car.make} ${car.model} (${car.year})`
+  car.chargeVoltage
+  if (typeof car.chargeVoltage !== 'undefined') {
+    str += `// ${car.chargeVoltage}v`
+  }
+  console.log(str)
 }
 
-/*
-//? A function that prints info about a car to stdout
-// function printCar(car: {
-//     make: string
-//     model: string
-//     year: number
-// }) {
-//     console.log(`${car.make} ${car.model} (${car.year})`)
-// }
+printCar(car)
 
-// printCar(car)
-
-/*
 //* Optional properties
 //? Insert into function printCar
 // let str = `${car.make} ${car.model} (${car.year})`
 // car.chargeVoltage
-// if (typeof car.chargeVoltage !== "undefined")
+// if (typeof car.chargeVoltage !== 'undefined')
 //   str += `// ${car.chargeVoltage}v`
 
-/*
-// printCar({ //? original fn works
-//     make: "Honda",
-//     model: "Accord",
-//     year: 2017,
-// })
+printCar({
+  //? original fn works
+  make: 'Honda',
+  model: 'Accord',
+  year: 2017,
+})
 
-// printCar({ //? optional property works too!
-//     make: "Tesla",
-//     model: "Model 3",
-//     year: 2020,
-//     chargeVoltage: 220,
-// })
+printCar({
+  //? optional property works too!
+  make: 'Tesla',
+  model: 'Model 3',
+  year: 2020,
+  chargeVoltage: 220,
+})
 
 /*
 //* Excess property checking
